e5caa57f9b09061fec29fa1ac5f2632cda5c1792
Dennisse Moreno C <deni.moreno11@gmail.com>
2018-07-26 10:33:16 -0400

    loading

--- src/app.js
+++ src/app.js
@@ -1,3 +1,39 @@
+var map = L.map('mapContainer').fitWorld();//consumiendo api de geolocalizacion leaveleft
 
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(map);
 
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
 
+    L.marker(e.latlng).addTo(map)
+    drawPlaces();
+}
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+
+map.locate({setView: true, maxZoom: 16});
+
+window.drawPlaces = (filter) => {
+    lat = -33.4883118;
+    long = -70.5100325;
+        var greenIcon = L.icon({
+        iconUrl: 'img/leaf-green.png',
+     shadowUrl: 'img/leaf-shadow.png',
+     iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    });
+}
